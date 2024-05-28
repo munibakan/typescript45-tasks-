e8028dbf1810df779d8efaf1e7a14abf8db07576
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 00:10:19 +0500

    Add files via upload
    TASK_14(MORE CONDITIONAL TEST)

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 "use strict";
-let car = 'subaru';
-console.log("\n -----> 5 tests evaluate to true :");
-//test 1:
-console.log("Is car == 'subaru'? I predict True .");
-console.log(car === 'subaru');
-//test:2
-console.log("Is car != 'toyota'? I predict True.");
-console.log(car !== 'toyota');
-//test 3:
-console.log("Is the length of car > 4? I predict True. ");
-console.log(car.length > 4);
-//test 4:
-console.log("Is the length of car <= 6? I predict True.");
-console.log(car.length <= 6);
-//test 5:
-console.log("Does car start with 's' ?  I predict True.");
-console.log(car.startsWith('s'));
-console.log("\n ----> 5 tests evaluate to false.");
-//test 1:
-console.log("Is car == 'honda'? I predict False.");
-console.log(car === 'honda');
-//test 2:
-console.log("Is car in uppercase? I predict False.");
-console.log(car.toUpperCase() === car);
-//test 3:
-console.log("Is car === 'Subaru'? I predict False.");
-console.log(car === 'Subaru');
-//test 4:
-console.log("Is car == 'Audi'? I predict False.");
-console.log(car === 'Audi');
-//test 5:
-console.log("Is the length of car === 7? I predict False.");
-console.log(car.length === 7);
+let str1 = 'Affan';
+let str2 = 'Sheikh';
+let num1 = 5;
+let num2 = 10;
+let Arr = [1, 2, 3, 4, 5];
+//Tests for equality and inequality with strings.
+console.log("Tests for equality and inequality of strings:");
+console.log(str1 === 'Affan');
+console.log(str1 === str2);
+//Tests using the lower case function
+console.log("\n Tests using the lower case function:");
+console.log(str1.toLowerCase() === 'Affan');
+console.log(str1.toLowerCase() !== 'sheikh');
+//Numerical Tests
+console.log("\nNumerical tests:");
+console.log(1 < 2);
+console.log(num1 >= num2);
+console.log(num1 === 5);
+console.log(num1 !== 10);
+//tests using 'and' & 'or' operators
+console.log("\nTests using 'and' & 'or' operators");
+console.log((num1 < 10) && (num2 > 5));
+console.log((num1 < 3) || (num2 > 15));
+//Tests whether an item is in an array 
+console.log("\n whether an item is an array:");
+console.log(Arr.includes(3));
+console.log(Arr.includes(6));
+//test whether an item is not in an array
+console.log("\n whether an item is not in an array:");
+console.log(Arr.includes(6));
+console.log(Arr.includes(2));
