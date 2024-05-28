7121673bdbf39d95322029a96153e49b8e127717
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-28 23:58:35 +0500

    Add files via upload
    Task_12

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 "use strict";
-//muniba khan
-// monday-23-24
-//storing friends names in an array//
-let names = ["Hira", "Fatima", "Hiba", "Minha"];
-//printing names one by one
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[4]);
+let names = ["Minha", "Ahmed", "khadija", "amna"];
+console.log(`Make Sure To Subscribe To Our Channel, ${names[0]}`);
+console.log(`Make Sure To Subscribe To Our Channel, ${names[1]}`);
+console.log(`Make Sure To Subscribe To Our Channel, ${names[2]}`);
+console.log(`Make Sure To Subscribe To Our Channel, ${names[3]}`);
