e210866ff4a4fee5aee2c96985de71f462a79679
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 00:01:53 +0500

    Add files via upload
    TASK_16(MORE GUEST)

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ console.log("\n New list of Invitation: \n");
 for (var j = 0; j < Guest_List.length; j++) {
     console.log("Dear ".concat(Guest_List[j], " : \n New list of Invitation \n"));
 }
+console.log("Good News! we have found a bigger dinner table,so more space is avaible. so our new guest are :\n");
+Guest_List.unshift("amna", "aaira", "haala");
+console.log(Guest_List);
+Guest_List.splice(2, 0, "aaira");
+console.log(Guest_List);
+Guest_List.push("haala");
+console.log(Guest_List);
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log("Dear ".concat(Guest_List[i], ": \n you are invited to dinner! \n"));
+}
