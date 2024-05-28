25f1750ad07658c02a5f8438290328f0c9307061
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-28 10:59:30 +0500

    Add files via upload
    TYPESCRIPT TASK 6

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var quote = "A person who never made a mistake never tried anything new";
-var famous_person = "Albert Einetein";
-var message = "".concat(famous_person, " once said , ").concat(quote);
-console.log(message);
+var personName = "\n\t Muniba Aijaz\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
