b833f444df3863090b7ce9189eaa6d11252239f0
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-28 11:20:32 +0500

    Add files via upload
    TYPESCRIPT(TASK -10) ADDING COMMENT

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var favouriteNumber = 7;
-console.log("My favourite number is ".concat(favouriteNumber));
+// muniba khan
+// monday, 22, 2024
+//store my favourite number in a variable
+var Favourite_Number = 99;
+//revealing my favourite number 
+var Message = ("My favourite number is ".concat(Favourite_Number));
+console.log(Message);
