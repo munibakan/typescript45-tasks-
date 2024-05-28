4e5e80fa4ae52b7036b33093eb3824e97b05752b
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 00:01:06 +0500

    Add files via upload
    TASK_15(CHANGE GUEST LIST)

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var Guest_List = ["Anum", "sara", "saba"];
+var Guest_List = ["anum", "saba", "hina"];
 for (var i = 0; i < Guest_List.length; i++) {
-    console.log("Dear ".concat(Guest_List[i], ":\n you are invited to dinner!\n"));
+    console.log("Dear ".concat(Guest_List[i], " : \n you are invited to Dinner! \n"));
+}
+console.log("\"unfortunately ".concat(Guest_List[1], ", can't come to dinner! \n\""));
+Guest_List[1] = "aizah";
+console.log("\n New list of Invitation: \n");
+for (var j = 0; j < Guest_List.length; j++) {
+    console.log("Dear ".concat(Guest_List[j], " : \n New list of Invitation \n"));
 }
