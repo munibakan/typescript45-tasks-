a49f491967f976866e168cb92eda75bdfd24f515
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 00:11:00 +0500

    Add files via upload
    TASK_25(ALIEN COLOURS)

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 "use strict";
-let str1 = 'Affan';
-let str2 = 'Sheikh';
-let num1 = 5;
-let num2 = 10;
-let Arr = [1, 2, 3, 4, 5];
-//Tests for equality and inequality with strings.
-console.log("Tests for equality and inequality of strings:");
-console.log(str1 === 'Affan');
-console.log(str1 === str2);
-//Tests using the lower case function
-console.log("\n Tests using the lower case function:");
-console.log(str1.toLowerCase() === 'Affan');
-console.log(str1.toLowerCase() !== 'sheikh');
-//Numerical Tests
-console.log("\nNumerical tests:");
-console.log(1 < 2);
-console.log(num1 >= num2);
-console.log(num1 === 5);
-console.log(num1 !== 10);
-//tests using 'and' & 'or' operators
-console.log("\nTests using 'and' & 'or' operators");
-console.log((num1 < 10) && (num2 > 5));
-console.log((num1 < 3) || (num2 > 15));
-//Tests whether an item is in an array 
-console.log("\n whether an item is an array:");
-console.log(Arr.includes(3));
-console.log(Arr.includes(6));
-//test whether an item is not in an array
-console.log("\n whether an item is not in an array:");
-console.log(Arr.includes(6));
-console.log(Arr.includes(2));
+let alien_color = "green";
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points");
+}
+alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points");
+}
+alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points!");
+}
