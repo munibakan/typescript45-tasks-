dff039453868e038f35e58a376576d3c61673b28
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 00:03:50 +0500

    Add files via upload
    TASK_18(VISIT 5 PLACES)

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 "use strict";
-let Guest_List = ["anum", "saba", "hina"];
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log(`Dear ${Guest_List[i]} : \n you are invited to Dinner! \n`);
-}
-console.log(`"unfortunately ${Guest_List[1]}, can't come to dinner! \n"`);
-Guest_List[1] = "aizah";
-console.log("\n New list of Invitation: \n");
-for (let j = 0; j < Guest_List.length; j++) {
-    console.log(`Dear ${Guest_List[j]} : \n New list of Invitation \n`);
-}
-console.log("Good News! we have found a bigger dinner table,so more space is avaible. so our new guest are :\n");
-Guest_List.unshift("amna");
-console.log(Guest_List);
-Guest_List.splice(2, 0, "aaira");
-console.log(Guest_List);
-Guest_List.push("haala");
-console.log(Guest_List);
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log(`Dear ${Guest_List[i]}: \n you are invited to dinner! \n`);
-}
-console.log("unfortunately, the new dinner table won't arrive in time, so we can only invite) two peple for a dinner.\n");
-while (Guest_List.length > 2) {
-    let notInvited = Guest_List.pop();
-    console.log(`Sorry, due to limited space we can't invite you on a dinner Miss. ${notInvited}\n`);
-}
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log(`Dear ${Guest_List[i]}: \n you are still invited to dinner! \n`);
-}
-Guest_List.pop();
-Guest_List.pop();
-console.log(Guest_List);
+let favouriteplaces = ["Switzerland", "Paris", "Norway", "Austrailia", "Europe"];
+console.log("original order" + favouriteplaces);
+console.log("alphabatical order" + [...favouriteplaces].sort());
+console.log("original order" + favouriteplaces);
+console.log("reverse alphabetical order" + [...favouriteplaces].sort().reverse());
+console.log("original order" + favouriteplaces);
+console.log("Reverse the order of list" + favouriteplaces.reverse());
+console.log("Reverse the order of list" + favouriteplaces.reverse());
+console.log("alphabatical order" + favouriteplaces.sort());
+console.log("alphabatical order" + favouriteplaces.sort().reverse());
