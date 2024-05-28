794b3545c24f93b36013ba9f56f100cd55231a1b
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 00:12:41 +0500

    Add files via upload
    TASK_21(ALIEN COLOUR S #3)

--- app.js
+++ app.js
@@ -2,16 +2,32 @@
 // 1st version
 let alien_color = 'green';
 if (alien_color === 'green') {
-    console.log("Congratulations! You just earned 5 points!");
+    console.log("Congratulations! You just earned 5 points.");
+}
+else if (alien_color === 'red') {
+    console.log("Congratulations! You just earned 10 points.");
+}
+else
+    console.log("Congratulations! You just earned 15 points.");
+// 2nd version
+alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 10 points.");
+}
+else if (alien_color === 'yellow') {
+    console.log("Congratulations! You just earned 10 points");
 }
 else {
-    console.log("Congratulations! You just earned 10 points!");
+    console.log("Congratulations! You just earned 15 points!");
 }
-//2nd version
+//3rd version
 alien_color = 'red';
 if (alien_color === 'green') {
-    console.log("Congratulations! You just earned 5 points!");
+    console.log("Congratulations! You just earned 5 points.");
+}
+else if (alien_color === 'yelloe') {
+    console.log("Congratulations! You just earned 10 points.");
 }
 else {
-    console.log("Congratulations! You just earned 10 points!");
+    console.log("Congratulations! You just earned 15 points.");
 }
