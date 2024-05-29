9c54b4ebd4efa5d185cf08ab7ae94007d62c5271
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 19:44:55 +0500

    Add files via upload
    TASK-37(LARGE SHIRT)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
-function make_shirt(size, message) {
-    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+function make_shirt(size = "large", message = "Ilove Typescript") {
+    console.log(`Making a ${size} t-shirt with the message "${message}" printd on it.`);
 }
-make_shirt("medium", "Code is Life");
+make_shirt("medium");
+make_shirt("small", "Dive into Coding");
