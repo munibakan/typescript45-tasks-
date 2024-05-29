86b1994e1b2d609a33a91e66413fc954f985ee0a
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 20:25:31 +0500

    Add files via upload
    TASK-43(UNCHANGED MAGICIAN)

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-"use strict";
-let magicians = ["don", "rameez", " chris"];
-function make_great(magicians) {
-    for (let i = 0; i < magicians.length; i++) {
-        magicians[i] = magicians[i] + "the great";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var Magician = ["Alice", "David", "chris"];
+function copyArray(arr) {
+    return __spreadArray([], arr, true);
+}
+function make_great(magicianArray) {
+    for (var i = 0; i < magicianArray.length; i++) {
+        Magician[i] = 'the great' + magicianArray[i];
     }
 }
-make_great(magicians);
-console.log(magicians);
+function show_magicians(magicians) {
+    magicians.forEach(function (element) {
+        console.log(element);
+    });
+}
+copyArray;
+make_great(Magician);
+show_magicians(Magician);
