1ab11744bb27d96004965566e8def2aee35b0f2d
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 19:44:12 +0500

    Add files via upload
    TASK-36(T-SHIRT)

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
-let animals = ["dog", "cat", "rabbit"];
-animals.forEach(animal => {
-    console.log(` A ${animal}would make a great pet.`);
-});
-console.log("Any of these animals would amke a great pet!");
+function make_shirt(size, message) {
+    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+}
+make_shirt("medium", "Code is Life");
