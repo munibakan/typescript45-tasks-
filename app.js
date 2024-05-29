af48a0e6ece183c5d1f94a8a62722d22e699f50f
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 20:29:39 +0500

    Add files via upload
    TASK-44(SANDWICH)

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+function make_sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var Magician = ["Alice", "David", "chris"];
-function copyArray(arr) {
-    return __spreadArray([], arr, true);
+    console.log("Making a sandwich with:".concat(items.join(','), "."));
 }
-function make_great(magicianArray) {
-    for (var i = 0; i < magicianArray.length; i++) {
-        Magician[i] = 'the great' + magicianArray[i];
-    }
-}
-function show_magicians(magicians) {
-    magicians.forEach(function (element) {
-        console.log(element);
-    });
-}
-copyArray;
-make_great(Magician);
-show_magicians(Magician);
+make_sandwich("ham", "cheese");
+make_sandwich("turkey", "lettuce", "tomato");
+make_sandwich("avacado", "sprouts", "mustard", "mayo");
