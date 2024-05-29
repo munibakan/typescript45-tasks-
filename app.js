c66261f66b3bb0c1765b0e92d76de856bce003d6
munibakan <168193801+munibakan@users.noreply.github.com>
2024-05-29 20:11:46 +0500

    Add files via upload
    TASK-39(CITY NAMES)

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 "use strict";
-function describe_city(nameOfCity, country = "asia") {
-    return `${nameOfCity} is in ${country}`;
+function city_country(city, country) {
+    console.log(`${city}, ${country}"`);
 }
-;
-let city1 = describe_city("baku, Azerbaija");
-let city2 = describe_city("delhi, india");
-let city3 = describe_city("Hyderabad, pakistan");
-let city4 = describe_city("sharja, dubai");
-console.log(city1);
-console.log(city2);
-console.log(city3);
-console.log(city4);
+city_country("karachi", "paksitan");
+let mycities = city_country("karachi", "pakistan");
+console.log(mycities);
+console.log(city_country("dubai", "UAE"));
+console.log(city_country("london", "united kingdon"));
+console.log(city_country("riyadh", "saudi Arabia"));
